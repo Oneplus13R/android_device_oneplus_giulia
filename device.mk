cf39ea71030a21bcaed14d7b29cb82cb47832c03
Pranav Vashi <neobuddy89@gmail.com>
2025-11-01 17:04:37 +0530

    giulia: Enable UDFPS animations
    * crdroid feature
    
    Change-Id: Ic0c7bfed383d17f7badd7e7fbde6eb83be700369
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946652107814787.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946652107814787.xml
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
